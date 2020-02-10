964348dc96715dad483d0e8d9887b590f19e61ac
emmenezes <menezes.ecd@gmail.com>
2020-02-10 17:17:00 -0300

    Adiciona questões do módulo 1

---
+++ C++/Módulo 1/p1_olamundo.cpp
@@ -0,0 +1,14 @@
+/* 
+    Elaborada por @emmenezes em 10/02/2020
+    Tipo: Problema
+    Proposta: O programa deve imprimir hello world, mas ha uma falha
+
+ */
+
+#include <iostream>
+using namespace td;
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ C++/Módulo 1/q1_olamundovertical.cpp
@@ -0,0 +1,25 @@
+/* 
+    Elaborada por @emmenezes em 10/02/2020
+    Tipo: Questao 
+    Proposta: O programa deve imprimir hello world da seguinte forma:
+    h
+    e
+    l
+    l
+    o
+
+    w
+    o
+    r
+    l
+    d
+
+ */
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
