0179fe719b89912e1f1f3e0406ae4cadf467a975
emmenezes <menezes.ecd@gmail.com>
2020-02-12 14:13:41 -0300

    Questão sobre entrada e saída de diferentes tipos

---
+++ recursos/C++/Módulo 1/p2_variostipos.cpp
@@ -0,0 +1,18 @@
+/* 
+    Elaborada por @emmenezes em 12/02/2020
+    Tipo: Problema
+    Proposta: Ler uma var tipo char, tipo int, tipo double e imprimir todas essas var em uma linha só
+
+ */
+
+#include <iostream>
+using namespace td;
+int main() 
+{
+    char carac;
+    int inteiro;
+    double real;
+    
+    cout << "Hello, World!";
+    return 0;
+}
